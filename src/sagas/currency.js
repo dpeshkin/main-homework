384f248a07535b3bc3a8a4ff65af6214482da8b9
DmitryPeshkin <Dmitrey04@mail.ru>
2017-12-14 12:21:36 +0500

    added currency saga tests / 3 task done

--- src/sagas/currency.js
+++ src/sagas/currency.js
@@ -23,12 +23,11 @@ import {
 } from "../actions/currency";
 import { candles } from "../api";
 
-function* fetchCurrencyFlow() {
+export function* fetchCurrencyFlow() {
   while (true) {
     const offset = yield select(getOffset);
     yield put(fetchBtcRequest(offset));
     yield put(fetchEthRequest(offset));
-
     yield delay(15000);
   }
 }
@@ -53,7 +52,7 @@ export function* currencyWatch() {
   }
 }
 
-function* fetchBtcFlow(action) {
+export function* fetchBtcFlow(action) {
   try {
     const response = yield call(candles, "btc", action.payload);
     yield put(fetchBtcSuccess(response.data.result));
@@ -62,7 +61,7 @@ function* fetchBtcFlow(action) {
   }
 }
 
-function* fetchEthFlow(action) {
+export function* fetchEthFlow(action) {
   try {
     const response = yield call(candles, "eth", action.payload);
     yield put(fetchEthSuccess(response.data.result));
